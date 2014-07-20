de8185d89350bb727599194d2c76bf69fc4f0951
ThundeR78 <julienwetstein@gmail.com>
2014-07-20 05:21:11 +0200

    Expandable GridView

--- app/src/main/java/fr/wetstein/mycv/fragment/GamesFragment.java
+++ app/src/main/java/fr/wetstein/mycv/fragment/GamesFragment.java
@@ -8,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.GridView;
 
 import java.util.List;
 
@@ -16,6 +15,7 @@ import fr.wetstein.mycv.R;
 import fr.wetstein.mycv.adapter.GridConsoleAdapter;
 import fr.wetstein.mycv.business.Console;
 import fr.wetstein.mycv.parser.GameParser;
+import fr.wetstein.mycv.view.ExpandableHeightGridView;
 
 /**
  * Created by ThundeR on 12/07/2014.
@@ -23,7 +23,7 @@ import fr.wetstein.mycv.parser.GameParser;
 public class GamesFragment extends Fragment implements AdapterView.OnItemClickListener {
     public static final String TAG = "GamesFragment";
 
-    private GridView gridView;
+    private ExpandableHeightGridView gridView;
 
     private List<Console> listConsole;
 
@@ -38,7 +38,7 @@ public class GamesFragment extends Fragment implements AdapterView.OnItemClickLi
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_games, container, false);
 
-        gridView = (GridView) rootView.findViewById(R.id.gridview_console);
+        gridView = (ExpandableHeightGridView) rootView.findViewById(R.id.gridview_console);
 
         return rootView;
     }
@@ -47,8 +47,13 @@ public class GamesFragment extends Fragment implements AdapterView.OnItemClickLi
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        GridConsoleAdapter gridAdapter = new GridConsoleAdapter(getActivity(), R.layout.gridview_console_item, listConsole);
+       initGridView(listConsole);
+    }
+
+    private void initGridView(List<Console> listItems) {
+        GridConsoleAdapter gridAdapter = new GridConsoleAdapter(getActivity(), R.layout.gridview_console_item, listItems);
         gridView.setAdapter(gridAdapter);
+        gridView.setExpanded(true);
         gridView.setOnItemClickListener(this);
     }
 
@@ -59,11 +64,12 @@ public class GamesFragment extends Fragment implements AdapterView.OnItemClickLi
         if (console != null) {
             int resIconId = getResources().getIdentifier(console.icon, "drawable", getActivity().getPackageName());
 
+            //Display Dialog with Games
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             builder.setTitle(console.shortName)
-                    .setIcon(resIconId)
-                    .setCancelable(true)
-                    .setItems(console.listGame.toArray(new CharSequence[console.listGame.size()]), null);
+                .setIcon(resIconId)
+                .setCancelable(true)
+                .setItems(console.listGame.toArray(new CharSequence[console.listGame.size()]), null);
 
             Dialog dialog = builder.create();
             dialog.show();
