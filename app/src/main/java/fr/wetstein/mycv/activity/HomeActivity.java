79cbb93f4a3c809fae413164d5808a30db0f3be5
ThundeR78 <julienwetstein@gmail.com>
2014-10-31 02:16:22 +0100

    Add IAP code

--- app/src/main/java/fr/wetstein/mycv/activity/HomeActivity.java
+++ app/src/main/java/fr/wetstein/mycv/activity/HomeActivity.java
@@ -2,8 +2,12 @@ package fr.wetstein.mycv.activity;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
@@ -24,6 +28,9 @@ import fr.wetstein.mycv.fragment.NewsListFragment;
 import fr.wetstein.mycv.fragment.ProfileFragment;
 import fr.wetstein.mycv.fragment.SkillListFragment;
 import fr.wetstein.mycv.fragment.StudyListFragment;
+import fr.wetstein.mycv.iap.IabHelper;
+import fr.wetstein.mycv.iap.IabResult;
+import fr.wetstein.mycv.iap.Purchase;
 import fr.wetstein.mycv.util.Actions;
 import fr.wetstein.mycv.util.PrefsManager;
 
@@ -34,6 +41,10 @@ public class HomeActivity extends Activity implements NavDrawerFragment.Navigati
     public static final String EXTRA_CONTENT = "CONTENT";
 
     //private Bundle mFragmentExtras;
+
+    //IAP
+    private IabHelper mHelper;
+    private static final int RC_REQUEST = 10001;
 
     /** Fragment managing the behaviors, interactions and presentation of the navigation drawer */
     private NavDrawerFragment mNavigationDrawerFragment;
@@ -63,6 +74,8 @@ public class HomeActivity extends Activity implements NavDrawerFragment.Navigati
 
         // Set up the drawer.
         mNavigationDrawerFragment.setUp(R.id.navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout));
+
+        initIAP();
     }
 
     @Override
@@ -94,6 +107,16 @@ public class HomeActivity extends Activity implements NavDrawerFragment.Navigati
     }
 
     @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (mHelper != null) {
+            mHelper.dispose();
+            mHelper = null;
+        }
+    }
+
+        @Override
     public void onNavigationDrawerItemSelected(int id, int position) {
         Fragment fragment = null;
         switch (id) {
@@ -146,7 +169,7 @@ public class HomeActivity extends Activity implements NavDrawerFragment.Navigati
     public boolean onCreateOptionsMenu(Menu menu) {
         if (!mNavigationDrawerFragment.isDrawerOpen()) {
             // Only show items in the action bar relevant to this screen if the drawer is not showing. Otherwise, let the drawer decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.home, menu);
+            getMenuInflater().inflate(R.menu.menu_home, menu);
             restoreActionBar();
             return true;
         }
@@ -178,9 +201,145 @@ public class HomeActivity extends Activity implements NavDrawerFragment.Navigati
             //Share message
             Actions.shareApp(this);
             return true;
+        } else
+        if (itemId == R.id.action_donate) {
+            displayDonationList();
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    private void initIAP() {
+        String base64EncodedPublicKey = getString(R.string.app_iap_key);
+        Log.d(TAG, "Creating IAB helper.");
+        mHelper = new IabHelper(this, base64EncodedPublicKey);
+
+        //Enable debug logging (false for production app)
+        mHelper.enableDebugLogging(MyCVApp.DEV_MODE);
+
+        Log.d(TAG, "Starting setup.");
+        // Start setup. This is asynchronous and the specified listener will be called once setup completes.
+        mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+            public void onIabSetupFinished(IabResult result) {
+                Log.d(TAG, "Setup finished.");
+
+                if (!result.isSuccess()) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.in_app_bill_error) + result, Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                //Have we been disposed of in the meantime? If so, quit.
+                if (mHelper == null)
+                    return;
+
+                // IAB is fully set up. Now, let's get an inventory of stuff we own.
+                //   --commented out here as we didn't need it for donation purposes.
+                // Log.d(TAG, "Setup successful. Querying inventory.");
+                // mHelper.queryInventoryAsync(mGotInventoryListener);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, "onActivityResult(" + requestCode + "," + resultCode + "," + data);
+        if (mHelper == null)
+            return;
+
+        // Pass on the activity result to the helper for handling
+        if (!mHelper.handleActivityResult(requestCode, resultCode, data)) {
+            //Not handled, so handle it ourselves (here's where you'd perform any handling of activity results not related to in-app billing...
+            super.onActivityResult(requestCode, resultCode, data);
+        } else {
+            Log.d(TAG, "onActivityResult handled by IABUtil.");
+        }
+    }
+
+    private void displayDonationList() {
+        final Activity activity = this;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.iap_choose_donation)
+                .setIcon(R.drawable.ic_action_good)
+                .setCancelable(true)
+                .setItems(getResources().getStringArray(R.array.iap_donation_array), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String payload = "";
+                        String iapId = getString((which == 0) ? R.string.iap_donate_tiny_id :
+                                (which == 1) ? R.string.iap_donate_small_id :
+                                (which == 2) ? R.string.iap_donate_medium_id :
+                                (which == 3) ? R.string.iap_donate_large_id :
+                                (which == 4) ? R.string.iap_donate_xlarge_id : 0);
+                        mHelper.launchPurchaseFlow(activity, iapId, RC_REQUEST, mPurchaseFinishedListener, payload);
+                    }
+                });
+
+        Dialog dialog = builder.create();
+        dialog.show();
+    }
+
+    /** Verifies the developer payload of a purchase. */
+    boolean verifyDeveloperPayload(Purchase p) {
+        String payload = p.getDeveloperPayload();
+
+        /**Follow google guidelines to create your own payload string here, in case it is needed.
+         *Remember it is recommended to store the keys on your own server for added protection
+         USE as necessary*/
+
+        return true;
+    }
+
+    // Callback for when a purchase is finished
+    IabHelper.OnIabPurchaseFinishedListener mPurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
+        public void onIabPurchaseFinished(IabResult result, Purchase purchase) {
+            Log.d(TAG, "Purchase finished: " + result + ", purchase: " + purchase);
+
+            if (mHelper == null)
+                return;
+
+            if (result.isFailure()) {
+                Toast.makeText(getApplicationContext(), getString(R.string.purchase_error) + result, Toast.LENGTH_LONG).show();
+                // setWaitScreen(false);
+                return;
+            }
+            if (!verifyDeveloperPayload(purchase)) {
+                Toast.makeText(getApplicationContext(), getString(R.string.error_verification), Toast.LENGTH_LONG).show();
+                // setWaitScreen(false);
+                return;
+            }
+
+            Log.d(TAG, "Purchase successful.");
+
+            if (purchase.getSku().equals(getString(R.string.iap_donate_tiny_id))
+                    || purchase.getSku().equals(getString(R.string.iap_donate_small_id))
+                    || purchase.getSku().equals(getString(R.string.iap_donate_medium_id))
+                    || purchase.getSku().equals(getString(R.string.iap_donate_large_id))
+                    || purchase.getSku().equals(getString(R.string.iap_donate_xlarge_id))) {
+                Log.d(TAG, "Purchase donation Ok");
+                mHelper.consumeAsync(purchase, mConsumeFinishedListener);
+            }
+
+        }
+    };
+
+    // Called when consumption is complete
+    IabHelper.OnConsumeFinishedListener mConsumeFinishedListener = new IabHelper.OnConsumeFinishedListener() {
+        public void onConsumeFinished(Purchase purchase, IabResult result) {
+            Log.d(TAG, "Consumption finished. Purchase: " + purchase + ", result: " + result);
+
+            if (mHelper == null)
+                return;
+
+            //Check which SKU is consumed here and then proceed
+            if (result.isSuccess()) {
+                Log.d(TAG, "Consumption successful. Provisioning.");
+                Toast.makeText(getApplicationContext(), getString(R.string.thank_you), Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), getString(R.string.error_consume) + result, Toast.LENGTH_LONG).show();
+            }
+
+
+            Log.d(TAG, "End consumption flow.");
+        }
+    };
 }
